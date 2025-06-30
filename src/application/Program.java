25ab5cbcd1016c95e5c1b506c099fa58da0b89b2
Hiran Ferreti Baccos - PrestServ <hiran.baccos.prestserv@petrobras.com.br>
2025-06-30 19:37:30 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Quantas pessoas serão digitadas? ");
+		int N = sc.nextInt();
+
+		String[] nomes = new String[N];
+		int[] idades = new int[N];
+		double[] alturas = new double[N];
+
+		for (int i = 0; i < N; i++) {
+			System.out.println("Dados da " + (i + 1) + "a pessoa:");
+			System.out.print("Nome: ");
+			nomes[i] = sc.next();
+			System.out.print("Idade: ");
+			idades[i] = sc.nextInt();
+			System.out.print("Altura: ");
+			alturas[i] = sc.nextDouble();
+		}
+
+		double soma = 0.0;
+		for (int i = 0; i < N; i++) {
+			soma += alturas[i];
+		}
+		double mediaAlturas = soma / N;
+
+		System.out.println();
+		System.out.printf("Altura média: %.2f%n", mediaAlturas);
+
+		int cont = 0;
+		for (int i = 0; i < N; i++) {
+			if (idades[i] < 16) {
+				cont++;
+			}
+		}
+		double percent = cont * 100.0 / N;
+
+		System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", percent);
+
+		for (int i = 0; i < N; i++) {
+			if (idades[i] < 16) {
+				System.out.println(nomes[i]);
+			}
+		}
+
+		sc.close();
+	}
+}
